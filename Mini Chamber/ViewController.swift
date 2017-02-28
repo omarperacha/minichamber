cbc57eb520ca4e62f86a4ccdf59daedc228c46c7
Omar Peracha <omar.peracha@gmail.com>
2017-02-28 11:58:25 +0000

    initial commit no framework

---
+++ Mini Chamber/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Mini Chamber
+//
+//  Created by Omar Peracha on 27/02/2017.
+//  Copyright © 2017 Omar Peracha. All rights reserved.
+//
+
+import UIKit
+import AudioKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
