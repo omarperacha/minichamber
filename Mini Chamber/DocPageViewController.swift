e65d39b484ccffad4159f81d118afeed21d47b00
Omar Peracha <omar.peracha@gmail.com>
2017-05-11 17:24:06 +0100

    print caf doc list

--- Mini Chamber/DocPageViewController.swift
+++ Mini Chamber/DocPageViewController.swift
@@ -12,6 +12,24 @@ class DocPageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Get the document directory url
+        let documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        do {
+            // Get the directory contents urls (including subfolders urls)
+            let directoryContents = try FileManager.default.contentsOfDirectory(at: documentsUrl, includingPropertiesForKeys: nil, options: [])
+            print(directoryContents)
+            
+            // if you want to filter the directory contents you can do like this:
+            let cafFiles = directoryContents.filter{ $0.pathExtension == "caf" }
+            print("caf urls:",cafFiles)
+            let cafFileNames = cafFiles.map{ $0.deletingPathExtension().lastPathComponent }
+            print("caf list:", cafFileNames)
+            
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
 
         // Do any additional setup after loading the view.
     }
